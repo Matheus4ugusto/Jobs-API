df861490bab9ec73497ac30e643a388b8550699f
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2025-02-28 15:21:54 -0300

    Inserindo autenticação no Swagger

--- src/main/java/br/com/matheus4ugusto/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/matheus4ugusto/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -2,7 +2,17 @@ package br.com.matheus4ugusto.gestao_vagas.modules.candidate.controllers;
 
 import br.com.matheus4ugusto.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.matheus4ugusto.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
+import br.com.matheus4ugusto.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
 import br.com.matheus4ugusto.gestao_vagas.modules.candidate.useCases.ProfileCandidateUseCase;
+import br.com.matheus4ugusto.gestao_vagas.modules.company.entities.JobEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +21,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -22,6 +33,9 @@ public class CandidateController {
 
     @Autowired
     private ProfileCandidateUseCase profileCandidateUseCase;
+
+    @Autowired
+    private ListAllJobsByFilterUseCase listAllJobsByFilterUseCase;
 
     @PostMapping
     public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
@@ -53,6 +67,22 @@ public class CandidateController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 
         }
+    }
+
+    @GetMapping("/job")
+    @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Listagem de vagas disponíveis para o candidato", description = "Essa função é responsável por listar todas as vagas disponíveis baseada no filtro")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(
+                            array = @ArraySchema(schema = @Schema(implementation = JobEntity.class))
+                    )
+            })
+    })
+    @SecurityRequirement(name = "jwt_auth")
+    public List<JobEntity> findJobByFilter(@RequestParam String filter) {
+        return this.listAllJobsByFilterUseCase.execute(filter);
     }
 
 }
