68e41a0720b8f919bf5467af67864e8e464e4e58
Matheus4ugusto <matheuspaulasoares10@gmail.com>
2025-02-28 14:54:00 -0300

    Configurando o Swagger]

--- src/main/java/br/com/matheus4ugusto/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/matheus4ugusto/gestao_vagas/security/SecurityConfig.java
@@ -21,6 +21,13 @@ public class SecurityConfig {
     @Autowired
     private SecurityCandidateFilter securityCandidateFilter;
 
+    private static final String[] SWAGGER_LIST = {
+       "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/swagger-resources/**"
+
+    };
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
@@ -28,7 +35,8 @@ public class SecurityConfig {
                     auth.requestMatchers("/candidate").permitAll()
                             .requestMatchers("/candidate/auth").permitAll()
                             .requestMatchers("/company").permitAll()
-                            .requestMatchers("/company/auth").permitAll();
+                            .requestMatchers("/company/auth").permitAll()
+                            .requestMatchers(SWAGGER_LIST).permitAll();
                     auth.anyRequest().authenticated();
                 }).addFilterBefore(securityCandidateFilter, BasicAuthenticationFilter.class)
                 .addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
